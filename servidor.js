dba2dbe3eb57b7cfceadd30ae365b2c4677a949a
Jose Maria Zamora <jzamora@biada.net>
2021-07-05 18:49:58 +0200

    conseguir que se consolee el onjeto como pide el enunciado, pero no sacarlo del map, solucion en progreso

--- servidor.js
+++ servidor.js
@@ -9,23 +9,11 @@ const urlApiLinias = `${urlApi}?app_key=${appKey}&app_id=${appId}`;
 const llamadaLinias = async () => {
   const response = await fetch(urlApiLinias);
   const linias = await response.json();
-  console.log(linias);
   return linias;
 };
-
-const app = express();
-
-const server = app.listen(4000, () => {
-  console.log("El servidor se ha iniciado.");
-});
-
-server.on("error", (error) => {
-  console.log(error);
-});
-
-app.get("/metro/lineas", async (req, res, next) => {
-  const respuesta = await llamadaLinias();
-  const infoLinias = respuesta.features.map((linia) => {
+const infoLinias = async () => {
+  const linias = await llamadaLinias();
+  const infoLinias = linias.features.map((linia) => {
     const {
       properties: { ID_LINIA, NOM_LINIA, DESC_LINIA },
     } = linia;
@@ -35,27 +23,59 @@ app.get("/metro/lineas", async (req, res, next) => {
       descripcion: DESC_LINIA,
     };
   });
-  res.json(infoLinias);
+  return infoLinias;
+};
+const verEstaciones = async (codigoLinia) => {
+  const apiEstaciones = `${urlApi}${codigoLinia}/estacions/?app_key=${appKey}&app_id=${appId}`;
+  const response = await fetch(apiEstaciones);
+  const estaciones = await response.json();
+  return estaciones;
+};
+
+const app = express();
+app.use(express.json());
+const server = app.listen(4000, () => {
+  console.log("El servidor se ha iniciado.");
 });
-/* "properties": {
-            "ID_LINIA": 12,
-            "CODI_LINIA": 91,
-            "NOM_LINIA": "L9S",
-            "DESC_LINIA": "Aeroport T1 - Zona Universitària",
-            "ORIGEN_LINIA": "Aeroport T1",
-            "DESTI_LINIA": "Zona Universitària",
-            "NUM_PAQUETS": 1,
-            "ID_OPERADOR": 1,
-            "NOM_OPERADOR": "Metro",
-            "NOM_TIPUS_TRANSPORT": "METRO",
-            "CODI_FAMILIA": 1,
-            "NOM_FAMILIA": "Metro",
-            "ORDRE_FAMILIA": 1,
-            "ORDRE_LINIA": 11,
-            "CODI_TIPUS_CALENDARI": "1",
-            "NOM_TIPUS_CALENDARI": "Tots els dies",
-            "DATA": "2021-07-04Z",
-            "COLOR_LINIA": "FB712B",
-            "COLOR_AUX_LINIA": "FB712B",
-            "COLOR_TEXT_LINIA": "FFFFFF"
-        } */
+
+server.on("error", (error) => {
+  console.log(error);
+});
+
+app.get("/metro/lineas", async (req, res, next) => {
+  const respuesta = await infoLinias();
+  res.json(respuesta);
+});
+
+app.get("/metro/linea", async (req, res, next) => {
+  const linias = await llamadaLinias();
+
+  const respuestaInfoLinias = await infoLinias();
+  const paradas = await linias.features.map(async (linia) => {
+    const {
+      properties: { NOM_LINIA, DESC_LINIA, CODI_LINIA },
+    } = linia;
+    const parada = await verEstaciones(CODI_LINIA);
+
+    const infoparada = parada.features.map((parada) => {
+      const {
+        properties: { ID_ESTACIO_LINIA, NOM_ESTACIO },
+      } = parada;
+      return { id: ID_ESTACIO_LINIA, nombre: NOM_ESTACIO };
+    });
+    console.log({
+      linea: NOM_LINIA,
+      descripcion: DESC_LINIA,
+
+      paradas: infoparada,
+    });
+    /// este return esta maldito, descubrir el por que
+    return {
+      linea: NOM_LINIA,
+      descripcion: DESC_LINIA,
+      paradas: infoparada,
+    };
+  });
+
+  res.json(paradas);
+});
